a1023c617a8fb544178a2bbba8279c5ba24ef585
User <huseynovali612@gmail.com>
2023-11-11 19:32:12 +0400

    change

--- server/Controller/UserController.js
+++ server/Controller/UserController.js
@@ -26,6 +26,53 @@ const userController = {
       res.status(500).json({ error: "Failed to add user" });
     }
   },
+
+  deleteUser: async (req, res) => {
+    const userId = req.params.id;
+    try {
+      const text = "DELETE FROM users WHERE id = $1 RETURNING *";
+      const values = [userId];
+
+      const data = await postgreSqlConnect.query(text, values);
+
+      if (data.rows.length === 0) {
+        res.status(404).json({ error: "User not found" });
+      } else {
+        res.json({
+          message: "User deleted successfully",
+          deletedUser: data.rows[0],
+        });
+      }
+    } catch (error) {
+      console.error("Error deleting user:", error);
+      res.status(500).json({ error: "Failed to delete user" });
+    }
+  },
+
+  updateUser: async (req, res) => {
+    const userId = req.params.id;
+    const { username, useremail, password } = req.body;
+
+    try {
+      const text =
+        "UPDATE users SET username = $1, useremail = $2, userpassword = $3 WHERE id = $4 RETURNING *";
+      const values = [username, useremail, password, userId];
+
+      const data = await postgreSqlConnect.query(text, values);
+
+      if (data.rows.length === 0) {
+        res.status(404).json({ error: "User not found" });
+      } else {
+        res.json({
+          message: "User updated successfully",
+          updatedUser: data.rows[0],
+        });
+      }
+    } catch (error) {
+      console.error("Error updating user:", error);
+      res.status(500).json({ error: "Failed to update user" });
+    }
+  },
 };
 
 module.exports = userController;
--- server/Router/usersRouter.js
+++ server/Router/usersRouter.js
@@ -5,5 +5,6 @@ const userRouter = express.Router();
 
 userRouter.get("/", userController.getAllUser);
 userRouter.post("/", userController.addUser);
-
+userRouter.delete("/:id", userController.deleteUser);
+userRouter.put('/:id',userController.updateUser)
 module.exports = userRouter;
