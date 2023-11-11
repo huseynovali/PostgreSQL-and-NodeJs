8a4671639222085b8e4d8831ccecb92cf93b04b7
User <huseynovali612@gmail.com>
2023-11-11 19:24:39 +0400

    add user controller

--- server/Controller/UserController.js
+++ server/Controller/UserController.js
@@ -11,6 +11,21 @@ const userController = {
       res.status(500).json({ error: "Failed to fetch users" });
     }
   },
+  addUser: async (req, res) => {
+    const { username, useremail, password } = req.body;
+    try {
+      const text =
+        "INSERT INTO users(username, useremail, userpassword) VALUES ($1, $2, $3) RETURNING *";
+      const values = [username, useremail, password];
+
+      const data = await postgreSqlConnect.query(text, values);
+
+      res.json(data.rows[0]); // Eklendikten sonra kullanıcının bilgilerini geri dönmek istiyorsanız.
+    } catch (error) {
+      console.error("Error adding user:", error);
+      res.status(500).json({ error: "Failed to add user" });
+    }
+  },
 };
 
 module.exports = userController;
--- server/Router/usersRouter.js
+++ server/Router/usersRouter.js
@@ -4,5 +4,6 @@ const userController = require("../Controller/UserController");
 const userRouter = express.Router();
 
 userRouter.get("/", userController.getAllUser);
+userRouter.post("/", userController.addUser);
 
 module.exports = userRouter;
